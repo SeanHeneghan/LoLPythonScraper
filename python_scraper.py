a10a8ca04762f1a13229abe7c123efdea09b0880
Sean Heneghan <sheneghan1@sheffield.ac.uk>
2020-07-27 17:44:29 +0100

    Pulled the god tier champs for each lane

--- python_scraper.py
+++ python_scraper.py
@@ -10,5 +10,23 @@ buffs_nerfs = soup.find('div', {'class': 'sch2'})
 
 print(buffs_nerfs.text)
 
-tier_list = soup.find(id='highlight-plugin')
-print(tier_list.prettify())
+#tier_html = soup.find(id='highlight-plugin')
+#print(tier_html.prettify())
+
+mid_lane = soup.find('tr', {'class': 'tiermid'}).findAll('td')
+print("MID LANE GODS: " + mid_lane[1].text)
+
+jungle = soup.find('tr', {'class': 'tierjungle'}).findAll('td')
+print("JUNGLE GODS: " + jungle[1].text)
+
+top_lane = soup.find('tr', {'class': 'tiertop'}).findAll('td')
+print("TOP LANE GODS: " + top_lane[1].text)
+
+adc = soup.find('tr', {'class': 'tieradc'}).findAll('td')
+print("BOT LANE GODS: " + adc[1].text)
+
+support = soup.find('tr', {'class': 'tiersupport'}).findAll('td')
+print("SUPPORT GODS: " + support[1].text)
+
+upcoming = soup.find('div', {'class': 'descText2'})
+print(upcoming.text)
