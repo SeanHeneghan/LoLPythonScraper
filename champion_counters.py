e437649c2bada0e3bbe00f48e63d0a5da92e3a5a
Sean Heneghan <sean.heneghan@outlook.com>
2020-07-31 16:46:14 +0100

    Pulling counters etc and printing to CSV, issue getting past chogath for some reason, will fix later.

--- champion_counters.py
+++ champion_counters.py
@@ -1,3 +1,4 @@
+import csv
 import requests as req
 from bs4 import BeautifulSoup as bs
 
@@ -12,3 +13,33 @@ all_champs = soup.find('div', {'class': 'champions'})
 champion_list = []
 for div in soup.findAll('div', {'style': 'background: #000;opacity: .8;padding: 2px;text-align: center;position: relative;top: 67px; font-size: 13px;color: #FFF;'}):
     champion_list.append(div.text)
+#print(champion_list)
+
+champion_page_list = []
+for champion in champion_list:
+    champion_page = f'https://lolcounter.com/champions/{champion}'
+    champion_page_list.append(champion_page)
+#print(champion_page_list)
+
+with open('champion_counters.csv', mode='a') as champ_file:
+        champ_file_writer = csv.writer(champ_file, dialect='excel')
+        header = ['Champion', 'Weak Against (Counter)', 'Strong Against']
+        champ_file_writer.writerow(header)
+
+        iterator = 0
+        for champion_link in champion_page_list:
+            champion_page = req.get(champion_link)
+            champion_soup = bs(champion_page.content, 'html.parser')
+            weak_against = champion_soup.find('div', {'class': 'weak-block'})
+            weak_list = []
+
+            for weak in weak_against.findAll('div', {'class': 'name'}):
+                weak_list.append(weak.text)
+            strong_against = champion_soup.find('div', {'class': 'strong-block'})
+            strong_list = []
+            for strong in strong_against.findAll('div', {'class': 'name'}):
+                strong_list.append(strong.text)
+            row = [champion_list[iterator],weak_list,strong_list]
+            champ_file_writer.writerow(row)
+            iterator+=1
+    
